b9d762d48624cf528c7effdaa8fec15af562e572
prasoonjha <38028423+prasoonjha@users.noreply.github.com>
2020-03-22 20:52:35 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -6,46 +6,42 @@ const authorInput = document.querySelector("#author");
 const pagesInput = document.querySelector("#pages");
 const statusSelect = document.querySelector("#status");
 
-submitBtn.onclick = (event) => {
-  event.preventDefault();
-  if(titleInput.value.trim().length == 0 || authorInput.value.trim().length == 0){
-    alert("Fill all the inputs");
-  }else{
-    addBook(new Book(titleInput.value, authorInput.value, pagesInput.value, (statusSelect.value == "true")));
-    titleInput.value = null;
-    authorInput.value = null;
-    pagesInput.value = null;
-    statusSelect.value = null;
-  }
-
-}
-
 function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.status = status;
   this.info = function() {
-    return this.title + ", " + this.pages + " pages, readed: " + this.read;
-  };
-  this.setStatus = function(){
-    this.status = !this.status
+     return this.title + ", " + this.pages + " pages, read: " + this.read;
+  }
+  this.setStatus = function() {
+    return this.status = !this.status;
   }
 }
 
-function addBook(book) {
-  BookList.push(book);
-  console.log("Book Created!", book);
-  render();
+submitBtn.onclick = function(event){
+  event.preventDefault();
+  addBook(new Book(titleInput.value, authorInput.value, pagesInput.value, (statusSelect.value == "true")));
+  titleInput.value = null;
+  authorInput.value = null;
+  pagesInput.value = null;
+  statusSelect.value = null;
 }
+
+function addBook(book) {
+    BookList.push(book);
+    console.log("Book Created!", book);
+    render();
+}
+
 
 
 function render() {
   tableBody.innerHTML = "";
   BookList.forEach((book, index) => {
-    let tr = document.createElement("tr");
+    var tr=document.createElement("tr");
 
-    let td = document.createElement("td");
+    var td=document.createElement("td");
     td.appendChild(document.createTextNode(index+1));
     tr.appendChild(td);
 
@@ -62,29 +58,27 @@ function render() {
     tr.appendChild(td);
 
     td = document.createElement("td");
-    //td.appendChild(document.createTextNode(book.status));
-    btn = document.createElement("button");
-    btn.classList.add("btn")
-    if(!book.status){
-      btn.classList.add("btn-outline-info")
-      btn.innerHTML = "Reading";
-    }else{
+    btn=document.createElement("button");
+    btn.classList.add("btn");
+    if(book.status){
       btn.classList.add("btn-success")
       btn.innerHTML = "Read";
     }
+    else{
+      btn.classList.add("btn-outline-info")
+      btn.innerHTML = "Reading";
+    }
     btn.onclick = () => {
-      console.log(book, index);
-      //BookList[index].setStatus();
       book.setStatus();
       render();
-    };
+    }
     td.appendChild(btn);
     tr.appendChild(td);
 
     td = document.createElement("td");
-    btn = document.createElement("button");
-    btn.classList.add("btn")
-    btn.classList.add("btn-danger")
+    btn=document.createElement("button");
+    btn.classList.add("btn");
+    btn.classList.add("btn-danger");
     btn.innerHTML = "<i class='fas fa-trash-alt'></i>";
     btn.onclick = () => {
       BookList.splice(index, 1);
@@ -93,6 +87,10 @@ function render() {
     td.appendChild(btn);
     tr.appendChild(td);
 
+
     tableBody.appendChild(tr);
+
+
   });
+
 }
